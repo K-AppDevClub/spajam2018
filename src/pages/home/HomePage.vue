57fa80da94ab69f4e12b6922a516430195da4a34
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-05-26 16:54:46 +0900

    router編集

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -11,7 +11,6 @@
   <v-ons-page>
     <navbar navType="blank" msg="シェイク"></navbar>
     <ons-card>
-      <h1>ユーザー名入力</h1>
       <v-ons-list-item>
         <span style="margin-right:15px;">ユーザー名: </span>
         <v-ons-input placeholder="（例：ほげほげ）" float v-model="user_name"></v-ons-input>
@@ -20,13 +19,13 @@
     <ons-card>
       <h1>ルームに参加</h1>
       <v-ons-list-item>
-        <span style="margin-right:15px;">ルーム名: </span>
-        <v-ons-input placeholder="（例：なんでも）" ></v-ons-input>
+        <span style="margin-right:15px;">ルームid: </span>
+        <v-ons-input placeholder="（例：なんでも）" float v-model="join_id"></v-ons-input>
       </v-ons-list-item>
-      <v-ons-button style="margin: 6px 0" @click="joinRoom()">はいる！</v-ons-button>
+      <v-ons-button style="margin: 6px 0" @click="joinGo()">はいる！</v-ons-button>
     </ons-card>
     <ons-card>
-      <h1>ルームを作る</h1>
+      <h1>ルームを作成</h1>
       <v-ons-button style="margin: 6px 0" @click="makeRoom()">つくる！</v-ons-button>
     </ons-card>
     <!-- <v-ons-fab @click="makeRoom()" style="position:fixed;" modifier="material" position="bottom right" >
@@ -43,7 +42,6 @@ import PlayRoom from '../../pages/play-room/PlayRoom';
 import RegionPage from '../../pages/region/Region';
 import DetailPlan from '../../pages/detail-plan/DetailPlan';
 import Navbar from '../../components/navbar/Navbar';
-import Chat from '../../components/chat/Chat';
 import Config from '../../config/Config';
 
 export default {
@@ -51,47 +49,40 @@ export default {
   components: {
     LoadingIndicator,
     Navbar,
-    Chat,
   },
   methods: {
-    goRoom() {
-      //localStorage.setItem('Name' , this.user_name);
+    goRoom(res) {
+      localStorage.setItem('Name' , this.user_name);
       this.$router.push({ name: 'play-room' ,params: { 
-        //room_id: res.data.id, 
-        room_id: 1,
-        user_name:'ehama'
-      } });
+        room_id: res.data.id, 
+        user_name:this.user_name
+      } })
     },
-    joinRoom(){
-      // this.axios.post(this.joinurl, {
-      //   name: this.room_id
-      // })
-      // .then(room_data => {
-      //   console.log(room_data)
-      //   this.goRoom(room_data)
-      // })
-      // .catch(err => {
-      //   this.$ons.notification.alert('ルームが存在しません');
-      // });
+    joinGo(){
+      this.axios.get(this.makeurl,{id:{}})
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem('Name' , this.user_name);
+        this.$router.push({ name: 'play-room' ,params: { 
+          room_id: this.join_id, 
+          user_name:this.user_name
+        } })
+      })
+      .catch(err => {
+        this.$ons.notification.alert('ルームが存在しません');
+      });
     },
 
     makeRoom(){
-      // this.axios.post(this.makeurl, {
-      //   room: {
-      //     name: this.room_id
-      //   }
-      // })
-      // .then(room_data => {
-      //   console.log(room_data)
-      //   this.goRoom(room_data)
-
-      // })
-      // .catch(err => {
-      //   this.$ons.notification.alert('そのルーム名はすでに使われています');
-      // });
-      this.goRoom();
-
-      
+      this.axios.post(this.makeurl,{room:{}})
+      .then(room_data => {
+        console.log(room_data)
+        this.goRoom(room_data)
+      })
+      .catch(err => {
+        this.$ons.notification.alert('そのルーム名はすでに使われています');
+      });
+ 
     },
     
   },
@@ -104,12 +95,13 @@ export default {
   },
   data() {
     return {
-      // joinurl: `http://k-appdev.com:3003/rooms/search`,
-      // makeurl:`http://k-appdev.com:3003/rooms`, 
+      makeurl:`https://k-appdev.com:3010/rooms`, 
+      //joinurl:`https://k-appdev.com:3010/rooms/${join_id}`,
       config: Config,
       room_id: '',
+      join_id: '',
       user_name: localStorage.getItem('Name'),
-      //strage_name:''
+      strage_name:''
     };
   },
   computed: {
